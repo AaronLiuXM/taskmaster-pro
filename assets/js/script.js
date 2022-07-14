67103fa901d878f337aff9b12b2793f2b0db2dfe
Aaron Liu <aaron155658@gmail.com>
2022-07-13 21:03:18 -0400

    add draggable and delete features

--- assets/js/script.js
+++ assets/js/script.js
@@ -53,6 +53,10 @@ $(".list-group").on("blur","textarea", function(){
   .attr("id")
   .replace("list-","");
 
+  var index = $(this)
+  .closest(".list-group-item")
+  .index();
+
   tasks[status][index].text = text;
   saveTasks();
 
@@ -137,8 +141,72 @@ $(".list-group").on("click", "p", function(){
 
 });
 
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event) {
+    console.log("activate", this);
+  },
+  deactivate: function(event) {
+    console.log("deactivate", this);
+  },
+  over: function(event) {
+    console.log("over", event.target);
+  },
+  out: function(event) {
+    console.log("out", event.target);
+  },
+  update: function(event) {
+    //array to store the task data in
+    var tempArr = [];
 
+    // loop over current set of children in sortable list
+    $(this).children().each(function() {
+      var text = $(this)
+        .find("p")
+        .text()
+        .trim();
+    
+      var date = $(this)
+        .find("span")
+        .text()
+        .trim();
 
+      // add task data to the temp array as an object
+      tempArr.push({
+        text:text,
+        date:date
+      });
+    });
+     
+    // trim down list's ID to match object property
+    var arrName = $(this)
+    .attr("id")
+    .replace("list-", "");
+
+    //update array on tasks object and save
+    tasks[arrName] = tempArr;  
+    
+    saveTasks();
+  
+  }});
+
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    ui.draggable.remove();
+    console.log("drop");
+  },
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event, ui) {
+    console.log("out");
+  }
+});
 
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {
@@ -170,9 +238,11 @@ $("#task-form-modal .btn-primary").click(function() {
       date: taskDate
     });
 
-    saveTasks();
+    saveTasks()
   }
 });
+
+
 
 // remove all tasks
 $("#remove-tasks").on("click", function() {
